05ea78819d564dc829fc70ecc6f5b3ac905d57ef
kshitijrajsharma <skshitizraj@gmail.com>
2026-01-11 16:01:45 +0100

    bump: version 0.1.2 → 0.1.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## v0.1.3 (2026-01-11)
+
+### Fix
+
+- **sync**: avoid adding optional deps in build
+
 ## v0.1.2 (2026-01-11)
 
 ### Fix
--- src/dl4cv_oda/__init__.py
+++ src/dl4cv_oda/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from .cleanup import clean_osm_data
 from .tiling import clip_labels_to_tiles, download_tiles
